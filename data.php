a2c2a6e4e129ffd170b4386d79d9b4256c8703cb
Philipp Scheit <p.scheit@ps-webforge.com>
2012-12-05 15:27:23 +0100

    local projects

--- data.php
+++ data.php
@@ -3,61 +3,50 @@
 $data = array();
 
 $data[] = array(
-  'label' => 'Project 1',
-  'start' => '2012-04-20', 
-  'end'   => '2012-05-12'
+  'label' => 'Hagedorn GmbH - Webauftritt',
+  'start' => '2012-12-01', 
+  'end'   => '2013-03-01'
 );
 
 $data[] = array(
-  'label' => 'Project 2',
-  'start' => '2012-04-22', 
-  'end'   => '2012-05-22'
+  'label' => 'Hagedorn GmbH - Pre-Seite',
+  'start' => '2012-12-01', 
+  'end'   => '2013-02-01',
+  'class' => 'important'
 );
 
 $data[] = array(
-  'label' => 'Project 3',
-  'start' => '2012-05-25', 
-  'end'   => '2012-06-20'
+  'label' => 'Rv Feen',
+  'start' => '2012-12-01', 
+  'end'   => '2013-01-15'
 );
 
 $data[] = array(
-  'label' => 'Project 4',
-  'start' => '2012-05-06', 
-  'end'   => '2012-06-17',
-  'class' => 'important',
-);
-
-$data[] = array(
-  'label' => 'Project 5',
-  'start' => '2012-05-11', 
-  'end'   => '2012-06-03', 
+  'label' => 'Rv Wimmelbuch',
+  'start' => '2012-12-07', 
+  'end'   => '2013-01-15',
   'class' => 'urgent',
 );
 
 $data[] = array(
-  'label' => 'Project 6',
-  'start' => '2012-05-15', 
-  'end'   => '2012-07-03'
+  'label' => 'Rv Musik',
+  'start' => '2013-01-01', 
+  'end'   => '2013-01-31',
+  'class' => 'important'
 );
 
 $data[] = array(
-  'label' => 'Project 7',
-  'start' => '2012-06-01', 
-  'end'   => '2012-07-03', 
-  'class' => 'important',
+  'label' => 'Rv Fahrzeuge',
+  'start' => '2012-12-15', 
+  'end'   => '2013-02-01'
 );
 
-$data[] = array(
-  'label' => 'Project 8',
-  'start' => '2012-06-01', 
-  'end'   => '2012-08-05'
-);
 
 $data[] = array(
-  'label' => 'Project 9',
-  'start' => '2012-07-22', 
-  'end'   => '2012-09-05',
-  'class' => 'urgent',
+  'label' => 'Rv Buchstaben',
+  'start' => '2013-01-01', 
+  'end'   => '2013-02-28'
 );
+
 
 ?>
